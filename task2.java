9e4b99743e11cb54534384837ec8ef553adee2d7
Viktor Vinogradov <viktor.vinogradov@list.ru>
2024-02-25 23:18:24 +0300

    Add tasks

---
+++ task2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.print(a * c + " " + b * c);
+    }
+}
